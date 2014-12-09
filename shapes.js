d2d96a12b7784968e3ca58d7e9aec2eec8476f9d
jtschoonhoven <jtschoonhoven@gmail.com>
2014-12-08 17:39:50 -0800

    added animation (dblclick)

--- shapes.js
+++ shapes.js
@@ -13,7 +13,10 @@
 
 
   // Create a constructor with an init function.
-  var Shapes = function() { this.initialize(); };
+  var Shapes = function() { 
+    this.animating = {};
+    this.initialize(); 
+  };
 
 
   // Set attributes to values in menu, then draw.
@@ -26,6 +29,12 @@
     // The longest side of the browser window;
     this.w = this.width >= this.height ? this.width : this.height;
 
+    // Draw a path between controll points.
+    this.showPath = !d3.select('#showPath:checked').empty();
+
+    // Mark each control point with a circle.
+    this.showCircles = !d3.select('#showCircles:checked').empty();
+
     // Separate each subsequent point by {n} degrees from the prev.
     this.step = parseInt(d3.select('#step').attr('value'));
 
@@ -34,9 +43,6 @@
 
     // Radius of control points.
     this.radii = parseFloat(d3.select('#radii').attr('value'));
-
-    // Radius of control points.
-    this.weight = parseFloat(d3.select('#weight').attr('value'));
 
     // How to curve lines between control points.
     this.interpolation = d3.select('#interpolation').attr('value');
@@ -50,9 +56,6 @@
     // Base pattern on which control points are aligned.
     this.pattern = d3.select('#pattern').attr('value');
 
-    this.canvas = d3.select('.canvas');
-    this.path = this.canvas.append('path');
-
     this.draw();
   };
 
@@ -64,37 +67,70 @@
   };
 
 
-  // Render SVG.
-  Shapes.prototype.draw = function() {
+  Shapes.prototype.animate = function(attribute, val, min, max, step) {
     var that = this;
 
+    min  = parseFloat(min);
+    max  = parseFloat(max);
+    val  = parseFloat(val);
+    step = parseFloat(step);
+
+    this.animating[attribute] ? this.animating[attribute] = false : this.animating[attribute] = true;
+
+    function animate() {
+      setTimeout(function() {
+        if (that.animating[attribute]) {
+          if (val + step > max) { step = step * -1; }
+          if (val + step < min) { step = step * -1; }
+          val = val + step;
+          that.update(attribute, val);
+          animate();
+        }
+      }, 200);
+    }
+
+    animate();
+  };
+
+
+  // Render SVG.
+  Shapes.prototype.draw = function() {
+
+    var that = this;
+    var data = new Array(this.points);
+    var canvas = d3.select('.canvas');
     this.radius = this.width * this.zoom / this.width;
 
-    // Create an array of length {points}.
-    var data = new Array(this.points);
 
-    var lineGenerator = d3.svg.line()
-      .x(function(d, i) { return patterns[that.pattern].x.call(that, i); })
-      .y(function(d, i) { return patterns[that.pattern].y.call(that, i); })
-      .interpolate(this.interpolation);
+    if (this.showCircles) {
+      var circles = canvas.selectAll('circle')
+        .data(d3.range(0, this.points));
 
-    // Circles show control points.
-    var circles = this.canvas.selectAll('circle')
-      .data(d3.range(0, this.points));
+      circles.exit().remove();
+      circles.enter().append('circle');
 
-    circles.exit().remove();
-    circles.enter().append('circle');
+      circles
+        .transition()
+        .attr('cx', function(d, i) { return patterns[that.pattern].x.call(that, i); })
+        .attr('cy', function(d, i) { return patterns[that.pattern].y.call(that, i); })
+        .attr('r', pow(this.radii, 2));
+    } else { canvas.selectAll('circle').remove(); }
 
-    circles
-      .transition()
-      .attr('cx', function(d, i) { return patterns[that.pattern].x.call(that, i); })
-      .attr('cy', function(d, i) { return patterns[that.pattern].y.call(that, i); })
-      .attr('r', pow(this.radii, 2));
 
-    this.path
-      .transition()
-      .style('stroke-width', this.weight)
-      .attr('d', function() { return lineGenerator(data); });
+    if (this.showPath) {
+      var path = canvas.selectAll('path').data([1])
+      path.enter().append('path');
+
+      var lineGenerator = d3.svg.line()
+        .x(function(d, i) { return patterns[that.pattern].x.call(that, i); })
+        .y(function(d, i) { return patterns[that.pattern].y.call(that, i); })
+        .interpolate(this.interpolation);
+
+      path.transition()
+        .attr('d', function() { return lineGenerator(data); });
+    } else { canvas.select('path').remove(); }
+
+
   };
 
 
